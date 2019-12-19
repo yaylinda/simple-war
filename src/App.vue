5f7abb58dc5de2f83aaf89b14b335f364e8d0839
yaylinda <lindazheng1993@gmail.com>
2019-12-18 23:42:46 -0600

    copy over css/styles

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,52 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
+@Component
+export default class App extends Vue {
+  public $material: any;
+
+  constructor() {
+    super();
+    console.log("App constructor");
+    console.log(`Running in ${process.env.NODE_ENV}`);
+    this.$material.theming.theme = "myTheme";
+  }
+
+  public mounted() {
+    console.log("App mounted");
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~vue-material/dist/theme/engine";
+
+@include md-register-theme(
+  "myTheme",
+  (
+    accent: #ff4495,
+    primary: #50e3c2,
+    theme: dark
+  )
+);
+
+@import "~vue-material/dist/theme/all";
+
+#app {
+  position: relative;
+  font-family: "Roboto Mono", sans-serif;
+  min-height: 100vh;
+}
 #nav {
   padding: 30px;
-
+  text-align: center;
   a {
     font-weight: bold;
-    color: #2c3e50;
-
     &.router-link-exact-active {
-      color: #42b983;
+      color: #ff4495;
     }
   }
 }
