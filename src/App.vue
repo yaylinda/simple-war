96ec281dd1bd65668fedb7ae94b830d5978b616a
yaylinda <lindazheng1993@gmail.com>
2019-12-20 01:57:13 -0600

    remove unused files; able to serve simple war correctly

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/simple-war">Simple War</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -47,15 +42,5 @@ export default class App extends Vue {
   position: relative;
   font-family: "Roboto Mono", sans-serif;
   min-height: 100vh;
-}
-#nav {
-  padding: 30px;
-  text-align: center;
-  a {
-    font-weight: bold;
-    &.router-link-exact-active {
-      color: #ff4495;
-    }
-  }
 }
 </style>
