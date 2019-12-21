dd4fe43a933aaf0702aa5e8ef5b98a19c4330702
yaylinda <lindazheng1993@gmail.com>
2019-12-21 00:30:01 -0600

    remove /dist from .gitignore

---
+++ dist/precache-manifest.3e3de64e767b275526c45531af3d26a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fad92e1d007a2761bd",
+    "url": "/css/app.612bfdc3.css"
+  },
+  {
+    "revision": "5a116b61aa2ad597f9a0",
+    "url": "/css/chunk-vendors.57f8e4c5.css"
+  },
+  {
+    "revision": "b41054a8a91158cf71184f2cfc600e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fad92e1d007a2761bd",
+    "url": "/js/app.54cd552e.js"
+  },
+  {
+    "revision": "5a116b61aa2ad597f9a0",
+    "url": "/js/chunk-vendors.d32a45cc.js"
+  },
+  {
+    "revision": "98a6ccba4d091ae6759f89249ffc263f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
